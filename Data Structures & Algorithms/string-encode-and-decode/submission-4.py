375c645e94f590970e520108c39335c0265af614
NavpreetKallah <129616341+NavpreetKallah@users.noreply.github.com>
2026-07-20 12:13:16 +0100

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        output = ""
+        for string in strs:
+            output += f"{len(string)}-{string}"
+        return output
+
+    def decode(self, s: str) -> List[str]:
+        output = []
+        strLength = ""
+        length = 0
+        word = ""
+        for letter in s:
+            if letter == "-":
+                length = int(strLength)
+                strLength = ""
+                output.append(word)
+                word = ""
+            elif length == 0:
+                strLength += letter
+            else:
+                length -= 1
+                word += letter
+        output.append(word)
+        output.pop(0)
+        return output
+
