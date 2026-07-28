10856144e460d0e4c4e866e126e7b8ec6439c772
NavpreetKallah <129616341+NavpreetKallah@users.noreply.github.com>
2026-07-28 18:10:41 +0100

    Add: time-based-key-value-store - submission-17

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-17.py
@@ -0,0 +1,51 @@
+class TimeMap:
+
+    """
+    
+    have a map of timestamps to maps
+    have a map of key to timestamps
+
+    if key is not in any timestamps it does not exist
+    if a key is not in the current timestamp then check the timestamp before it
+
+    This works though try to optimise for space next time
+    
+    """
+
+    def __init__(self):
+        self.timestampToMap = {}
+        self.keyToTimestamp = {}
+        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if timestamp not in self.timestampToMap:
+            self.timestampToMap[timestamp] = {}
+
+        timestampMap = self.timestampToMap[timestamp]
+        timestampMap[key] = value
+
+        if key not in self.keyToTimestamp:
+            self.keyToTimestamp[key] = []
+        
+        self.keyToTimestamp[key].append(timestamp)
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.keyToTimestamp:
+            return ""
+        timestamps = self.keyToTimestamp[key]
+
+        l = 0
+        r = len(timestamps) - 1
+        biggest = -1
+
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if timestamps[mid] > timestamp:
+                r = mid - 1
+            else:
+                biggest = timestamps[mid]
+                l = mid + 1
+
+        return self.timestampToMap[biggest][key] if biggest != -1 else ""
+        
