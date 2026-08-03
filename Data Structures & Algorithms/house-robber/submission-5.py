01088d8f0995151968426f84ab526f7f87e62a9a
NavpreetKallah <129616341+NavpreetKallah@users.noreply.github.com>
2026-08-03 20:22:16 +0100

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+
+        for index, num in enumerate(nums):
+            if index >= 3:
+                dp[index] += max(dp[index - 2], dp[index - 3]) + num
+            elif index == 2:
+                dp[index] = num + dp[index - 2]
+            else:
+                dp[index] = num
+
+        return dp[-1] if len(dp) == 1 else max(dp[-1], dp[-2])
+        
