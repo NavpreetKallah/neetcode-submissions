24a8bfecc4e66c511de342a056870de7d9686ac4
NavpreetKallah <129616341+NavpreetKallah@users.noreply.github.com>
2026-09-04 13:59:42 +0100

    Add: jump-game - submission-7

---
+++ Data Structures & Algorithms/jump-game/submission-7.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    """
+    
+    [1,2,1,0,1]
+
+    to get to the end we require atleast 1 at position 4
+    if this is valid then we require atleast 1 at position 3
+    and so forth
+
+    if there is no 1 at position 4
+    we then require atleast a 2 at position 3
+    if this is not atleast 2 then we require atleast 3 at position 2
+    
+    """
+
+
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+        curr = 1
+        for idx, num in enumerate(nums[::-1]):
+            if idx == 0:
+                continue
+            if num >= curr:
+                curr = 1
+            else:
+                curr += 1
+            
+        return nums[0] >= curr
