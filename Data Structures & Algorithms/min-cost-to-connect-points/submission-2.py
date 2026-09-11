e0d5ab54105df42ca291048a9806ca2f86cc7424
NavpreetKallah <129616341+NavpreetKallah@users.noreply.github.com>
2026-09-11 12:38:26 +0100

    Add: min-cost-to-connect-points - submission-2

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-2.py
@@ -0,0 +1,60 @@
+import heapq
+from typing import List
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, i: int) -> int:
+        if self.parent[i] == i:
+            return i
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i: int, j: int) -> bool:
+        root_i = self.find(i)
+        root_j = self.find(j)
+        
+        if root_i == root_j:
+            return False
+        
+        if self.rank[root_i] < self.rank[root_j]:
+            self.parent[root_i] = root_j
+        elif self.rank[root_i] > self.rank[root_j]:
+            self.parent[root_j] = root_i
+        else:
+            self.parent[root_j] = root_i
+            self.rank[root_i] += 1
+            
+        return True
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        if n <= 1:
+            return 0
+
+        edges = []
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                d = abs(x2 - x1) + abs(y2 - y1)
+                edges.append((d, i, j))
+        
+        edges.sort()
+
+        dsu = UnionFind(n)
+        res = 0
+        connections = 0
+
+        for cost, u, v in edges:
+            if dsu.union(u, v):
+                res += cost
+                connections += 1
+                if connections == n - 1:
+                    break
+
+        return res
